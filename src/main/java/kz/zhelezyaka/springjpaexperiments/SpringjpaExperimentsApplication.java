dcf1a1e08732a50f6a2b334bccb3b5f3a2ba9f7f
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-05-28 11:48:03 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/springjpaexperiments/SpringjpaExperimentsApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springjpaexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpaExperimentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/zhelezyaka/springjpaexperiments/SpringjpaExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springjpaexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpaExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
