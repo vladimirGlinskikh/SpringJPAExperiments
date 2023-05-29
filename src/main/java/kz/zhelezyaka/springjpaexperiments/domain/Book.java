08215bd0561b23f4724400ea127fcbee21717419
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-05-29 12:42:01 +0300

    JPA Entities

---
+++ src/main/java/kz/zhelezyaka/springjpaexperiments/domain/Book.java
@@ -0,0 +1,26 @@
+package kz.zhelezyaka.springjpaexperiments.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String isbn;
+    private String publisher;
+}
