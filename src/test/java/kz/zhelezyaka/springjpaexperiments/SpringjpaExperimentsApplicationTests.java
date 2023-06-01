40ab461d22183209a5b286752efa45cb895edadc
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-06-01 12:48:04 +0300

    Spring Boot Test

--- src/test/java/kz/zhelezyaka/springjpaexperiments/SpringjpaExperimentsApplicationTests.java
+++ src/test/java/kz/zhelezyaka/springjpaexperiments/SpringjpaExperimentsApplicationTests.java
@@ -1,13 +1,26 @@
 package kz.zhelezyaka.springjpaexperiments;
 
+import kz.zhelezyaka.springjpaexperiments.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringjpaExperimentsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testBookRepository() {
+        long count = bookRepository.count();
+        assertThat(count).isGreaterThan(0);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
