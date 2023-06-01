74e38cd3a41d3a600d34cc1081a34809bc945e22
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-06-01 21:11:58 +0300

    Test Transactions

--- src/test/java/kz/zhelezyaka/springjpaexperiments/SpringBootJpaTestSlice.java
+++ src/test/java/kz/zhelezyaka/springjpaexperiments/SpringBootJpaTestSlice.java
@@ -2,23 +2,39 @@ package kz.zhelezyaka.springjpaexperiments;
 
 import kz.zhelezyaka.springjpaexperiments.domain.Book;
 import kz.zhelezyaka.springjpaexperiments.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
 
     @Autowired
     BookRepository bookRepository;
 
+    @Commit
+    @Order(1)
     @Test
     void testJpaTestSplice() {
         long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(0);
         bookRepository.save(new Book(4L, "Effective Java", "234298509", "Wesley"));
         long countAfter = bookRepository.count();
         assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaSpliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
     }
 }
